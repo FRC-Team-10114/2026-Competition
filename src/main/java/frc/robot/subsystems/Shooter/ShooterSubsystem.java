a2eb07f5008a30d466879dd44a1ccb0a1101008f
YuanChi Hsieh <60728633+boring89@users.noreply.github.com>
2026-01-14 16:47:07 +0800

    fix something

--- src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -7,7 +7,6 @@ import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.Shooter.Hood.HoodIO;
-import frc.robot.subsystems.Shooter.Hood.HoodIO.HoodIOInputs;
 import frc.robot.subsystems.Shooter.Roller.RollerIO;
 import frc.robot.subsystems.Shooter.Turret.TurretIO;
 
@@ -16,21 +15,17 @@ public class ShooterSubsystem extends SubsystemBase {
     private final HoodIO hood;
     private final RollerIO roller;
     private final TurretIO turret;
-    private final HoodIOInputs hoodIOInputs;
 
     public ShooterSubsystem(HoodIO hood, RollerIO roller, TurretIO turret) {
 
         this.hood = hood;
         this.roller = roller;
         this.turret = turret;
-        this.hoodIOInputs = new HoodIOInputs();
     }
 
 
     @Override
-    public void periodic() {
-        hood.updateInputs(hoodIOInputs);
-    }
+    public void periodic() {}
 
     public void setHoodAngle(double targetRad) {
         this.hood.setAngle(targetRad); 
