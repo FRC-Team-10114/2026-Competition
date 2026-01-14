9e64379e692205a8d8014ebffbe86b31f23ea2a9
YuanChi Hsieh <60728633+boring89@users.noreply.github.com>
2026-01-14 16:45:07 +0800

    Add ShooterSubsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,22 +1,26 @@
 package frc.robot;
 
-
 public class Constants {
 
     public static final class SwerveModuleConstants {
         public static final String[] ModuleName = {
-            "ForntLeft", 
-            "FrontRight", 
-            "BackLeft", 
-            "BackRight"
+                "ForntLeft",
+                "FrontRight",
+                "BackLeft",
+                "BackRight"
         };
     }
-    
+
     public static final class IntakeConstants {
         public static final int INTAKE_MOTOR_ID = 13;
     }
 
     public static final class VisionConstants {
         public static final double MAX_GYRO_RATE = 1080;
+    }
+
+    public static final class HoodConstants {
+        private final double GEAR_RATIO = 1; // 假設減速比 50:1
+        private final double POSITION_CONVERSION_FACTOR = (2 * Math.PI) / GEAR_RATIO;
     }
 }
---
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterSubsystem.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems.Shooter;
+
+import java.util.function.Supplier;
+
+
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.subsystems.Shooter.Hood.HoodIO;
+import frc.robot.subsystems.Shooter.Hood.HoodIO.HoodIOInputs;
+import frc.robot.subsystems.Shooter.Roller.RollerIO;
+import frc.robot.subsystems.Shooter.Turret.TurretIO;
+
+public class ShooterSubsystem extends SubsystemBase {
+    
+    private final HoodIO hood;
+    private final RollerIO roller;
+    private final TurretIO turret;
+    private final HoodIOInputs hoodIOInputs;
+
+    public ShooterSubsystem(HoodIO hood, RollerIO roller, TurretIO turret) {
+
+        this.hood = hood;
+        this.roller = roller;
+        this.turret = turret;
+        this.hoodIOInputs = new HoodIOInputs();
+    }
+
+
+    @Override
+    public void periodic() {
+        hood.updateInputs(hoodIOInputs);
+    }
+
+    public void setHoodAngle(double targetRad) {
+        this.hood.setAngle(targetRad); 
+    }
+
+    public void setTurretAngle(Supplier<Angle> targetRad) {
+        this.turret.setControl(targetRad);
+    }
+
+    public void setRollerVelocity(AngularVelocity velocity) {
+        this.roller.setRPM(velocity);
+    }
+}
